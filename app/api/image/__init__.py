06a3d7d09fde81e3e16e43806afa2a99422144df
Eojin K1m <eojin.dev@gmail.com>
2020-03-28 19:02:05 +0900

    add image delete

--- app/api/image/__init__.py
+++ app/api/image/__init__.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from app.api.image.view import ImageUpload
+from app.api.image.view import ImageUpload, Image
 
 image_blueprint = Blueprint('image api', 'image api')
 image_api = Api(image_blueprint)
 image_api.add_resource(ImageUpload, '/')
+image_api.add_resource(Image, '/<id>')
 
 
--- app/api/image/view.py
+++ app/api/image/view.py
@@ -1,4 +1,4 @@
-from flask import request, make_response
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 
 from app.api.image.service import ImageService
@@ -7,4 +7,11 @@ from app.api.image.service import ImageService
 class ImageUpload(Resource):
     def post(self):
         return make_response(ImageService.image_upload(request.files['image']))
+
+class Image(Resource):
+    def delete(self, id):
+        if not ImageService.delete_image(id):
+            return make_response(jsonify(msg='image not found'), 404)
+        else:
+            return make_response(jsonify(msg='image deleted'), 200)
 
