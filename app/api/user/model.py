0d63a9ddd6b1e67d300d65fc7d08ff2fab58f4ab
Eojin K1m <eojin.dev@gmail.com>
2020-04-03 18:20:36 +0900

    from now, posts arent queried when query user info Fix AccountModel.get_account_by_email() calling

--- app/api/user/model.py
+++ app/api/user/model.py
@@ -25,8 +25,6 @@ class UserSchema(ma.SQLAlchemySchema):
     username = ma.auto_field()
     explain = ma.auto_field()
     profile_image = ma.Nested('ImageSchema', only=["url"])
-    posts = ma.Nested('PostSchema', many=True, only=["title", "uploader", "id"])
-
 
 
 user_schema = UserSchema()
