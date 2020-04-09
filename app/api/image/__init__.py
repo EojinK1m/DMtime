f0c395bb0e12057b727d27621420eb3c89ce6240
EojinKim <eojin.dev@gmail.com>
2020-04-09 17:35:13 +0900

    temp

--- app/api/image/__init__.py
+++ app/api/image/__init__.py
@@ -5,7 +5,7 @@ from app.api.image.view import ImageUpload, Image
 
 image_blueprint = Blueprint('image api', 'image api')
 image_api = Api(image_blueprint)
-image_api.add_resource(ImageUpload, '/')
+image_api.add_resource(ImageUpload, '')
 image_api.add_resource(Image, '/<id>')
 
 
