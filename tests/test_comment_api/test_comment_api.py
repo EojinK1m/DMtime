3ab0e2026b9f25fcb993c6a748deecf0f8456ba4
Eojin K1m <eojin.dev@gmail.com>
2020-05-19 19:08:02 +0900

    make test for comment API delete method

--- tests/test_comment_api/test_comment_api.py
+++ tests/test_comment_api/test_comment_api.py
@@ -113,3 +113,77 @@ def test_comment_put_to_not_exist_comment(client,
                     headers = {'authorization':'Bearer '+temp_account.generate_access_token()})
 
     assert rv.status_code == 404
+
+
+def test_comment_delete_correct(client,
+                                create_temp_account,
+                                create_temp_gallery,
+                                create_temp_post,
+                                create_temp_comment):
+    temp_account = create_temp_account()
+    temp_gallery = create_temp_gallery()
+    temp_post = create_temp_post(temp_account.id, temp_gallery.id)
+    temp_comment = create_temp_comment(temp_account.id, temp_post.id)
+
+
+    rv = client.delete(url+f'{temp_comment.id}',
+                    headers = {'authorization':'Bearer '+temp_account.generate_access_token()})
+
+    assert rv.status_code == 200
+
+
+def test_comment_delete_with_admin_account(client,
+                                            create_temp_account,
+                                            create_temp_gallery,
+                                            create_temp_post,
+                                            create_temp_comment):
+    temp_account = create_temp_account()
+    admin_account = create_temp_account(is_admin=True)
+    temp_gallery = create_temp_gallery()
+    temp_post = create_temp_post(temp_account.id, temp_gallery.id)
+    temp_comment = create_temp_comment(temp_account.id, temp_post.id)
+
+    rv = client.delete(url + f'{temp_comment.id}',
+                    headers={'authorization': 'Bearer ' + admin_account.generate_access_token()})
+
+    assert rv.status_code == 200
+
+def test_comment_delete_with_another_account(client,
+                                            create_temp_account,
+                                            create_temp_gallery,
+                                            create_temp_post,
+                                            create_temp_comment):
+    temp_account = create_temp_account()
+    temp_account_2 = create_temp_account()
+    temp_gallery = create_temp_gallery()
+    temp_post = create_temp_post(temp_account.id, temp_gallery.id)
+    temp_comment = create_temp_comment(temp_account.id, temp_post.id)
+
+    rv = client.delete(url + f'{temp_comment.id}',
+                    headers={'authorization': 'Bearer ' + temp_account_2.generate_access_token()})
+
+    assert rv.status_code == 403
+
+def test_comment_delete_not_exist_comment(client,
+                                            create_temp_account):
+    temp_account = create_temp_account()
+
+    rv = client.delete(url + f'{1}',
+                    headers={'authorization': 'Bearer ' + temp_account.generate_access_token()})
+
+    assert rv.status_code == 404
+
+
+def test_comment_delete_without_access_token(client,
+                                            create_temp_account,
+                                            create_temp_gallery,
+                                            create_temp_post,
+                                            create_temp_comment):
+    temp_account = create_temp_account()
+    temp_gallery = create_temp_gallery()
+    temp_post = create_temp_post(temp_account.id, temp_gallery.id)
+    temp_comment = create_temp_comment(temp_account.id, temp_post.id)
+
+    rv = client.delete(url + f'{temp_comment.id}')
+
+    assert rv.status_code == 401
