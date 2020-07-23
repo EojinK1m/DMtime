ac7de8355aed6f829951ed539697f48757ab2dff
EoJin <eojin.dev@gmail.com>
2020-07-23 19:54:09 +0900

    [CREATE] </app/config.py>

---
+++ app/config.py
@@ -0,0 +1,32 @@
+import os
+
+class DefaultConfig(object):
+    DEBUG = False
+    TESTING = False
+
+    DB_USER = os.environ['DMTIME_DB_USER']
+    DB_PW = os.environ['DMTIME_DB_PW']
+    DB_URI = os.environ['DMTIME_DB_URI']
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB_URI}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    IMAGE_UPLOADS = os.environ['DMTIME_IMAGE_STORAGE']
+    IMAGES_URL = os.environ['DMTIME_SERVER_NAME']+'/images/'
+    
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+class ProductConfig(DefaultConfig):
+    pass
+
+class DevelopConfig(DefaultConfig):
+    DEBUG = True
+    TESTING = True
+    JWT_SECRET_KEY = os.environ['DMTIME_JWT_KEY']
+
+    ADMIN_LIST = ['eojin.dev@gmail.net', 'vjslzhs6@naver.com']
+
+class TestConfig(DefaultConfig):
+    DEBUG = True
+    TESTING = True
+    JWT_SECRET_KEY = 'Fuck im lonely'
+    ADMIN_LIST = ['test_admin']
