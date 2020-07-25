806908df85b5416d6608334244c671221b507093
EoJin <eojin.dev@gmail.com>
2020-07-25 19:19:17 +0900

    [UPDATE] </docker-entrypoint.sh> add verify_env func

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+required_environment_variables=("DMTIME_DB_URI" "DMTIME_DB_USER" "DMTIME_DB_PW" "DMTIME_JWT_KEY" "DMTIME_IMAGE_STORAGE" "DMTIME_SERVER_NAME")
+
+
+
+verify_required_env() {
+    for require_var_name in "${required_environment_variables[@]}"; do
+        if [[ -z "$"$require_var_name ]]; then
+            printf '%s is not found. Check environment.\n' $require_var_name
+            exit 1
+        fi
+    done
+}       
+
+verify_required_env
+
+ 
