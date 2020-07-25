520873f28ccf2831345c4d4b51d365664293061b
EoJin <eojin.dev@gmail.com>
2020-07-25 19:20:34 +0900

    [UPDATE] </app/config.py> JWT_SERCET_KEY var mv to DefaultConfig

--- app/config.py
+++ app/config.py
@@ -10,9 +10,10 @@ class DefaultConfig(object):
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB_URI}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    JWT_SECRET_KEY = os.environ['DMTIME_JWT_KEY']
+
     IMAGE_UPLOADS = os.environ['DMTIME_IMAGE_STORAGE']
     IMAGES_URL = os.environ['DMTIME_SERVER_NAME']+'/images/'
-    
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 class ProductConfig(DefaultConfig):
@@ -21,7 +22,6 @@ class ProductConfig(DefaultConfig):
 class DevelopConfig(DefaultConfig):
     DEBUG = True
     TESTING = True
-    JWT_SECRET_KEY = os.environ['DMTIME_JWT_KEY']
 
     ADMIN_LIST = ['eojin.dev@gmail.net', 'vjslzhs6@naver.com']
 
