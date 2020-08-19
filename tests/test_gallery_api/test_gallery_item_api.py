db21a9945b7c84aec77186e06a23113e32fcea86
EoJin <eojin.dev@gmail.com>
2020-08-20 01:42:09 +0900

    [MIDIFY] fix typo of package name

--- tests/test_gallery_api/test_gallery_item_api.py
+++ tests/test_gallery_api/test_gallery_item_api.py
@@ -2,15 +2,16 @@ url = '/api/board/galleries/'
 
 
 def test_gallery_delete_correct(client, create_temp_account, create_temp_gallery):
-    admin_account = create_temp_account(is_admin = True)
-    temp_gallery = create_temp_gallery()
+    temp_account = create_temp_account()
+    temp_gallery = create_temp_gallery(temp_account)
 
     rv = client.delete(url+f'{temp_gallery.id}',
-                       headers={'authorization':'Bearer '+admin_account.generate_access_token()})
+                       headers={'authorization':'Bearer '+temp_account.generate_access_token()})
 
     assert rv.status_code == 200
 
-def test_gallery_delete_with_not_admin_account(client, create_temp_account, create_temp_gallery):
+
+def test_gallery_delete_without_authority(client, create_temp_account, create_temp_gallery):
     temp_account = create_temp_account()
     temp_gallery = create_temp_gallery()
 
@@ -18,6 +19,7 @@ def test_gallery_delete_with_not_admin_account(client, create_temp_account, crea
                        headers={'authorization': 'Bearer ' + temp_account.generate_access_token()})
 
     assert rv.status_code == 403
+
 
 def test_gallery_patch_correct(client, create_temp_account, create_temp_gallery):
     admin_account = create_temp_account(is_admin = True)
@@ -32,6 +34,7 @@ def test_gallery_patch_correct(client, create_temp_account, create_temp_gallery)
 
     assert rv.status_code == 200
 
+8
 def test_gallery_patch_with_data_miss(client, create_temp_account, create_temp_gallery):
     admin_account = create_temp_account(is_admin = True)
 
@@ -44,23 +47,21 @@ def test_gallery_patch_with_data_miss(client, create_temp_account, create_temp_g
 
     assert rv.status_code == 200
 
-    changing_gallery_info_name_missed= {'explain':'this is explain of changed gallery'}
+    changing_gallery_info_name_missed = {'explain':'this is explain of changed gallery'}
     rv_2 = client.patch(url + f'{temp_gallery.id}',
                         headers={'authorization': 'Bearer ' + admin_account.generate_access_token()},
                         json=changing_gallery_info_name_missed)
 
     assert rv_2.status_code == 200, rv_2.json['msg']
 
-def test_gallery_patch_with_not_admin_account(client, create_temp_account, create_temp_gallery):
-    temp_account = create_temp_account(is_admin = False)
-    temp_gallery = create_temp_gallery()
 
-    changing_gallery_info = {'name':'change_gallery_info',
-                             'explain':'this is explain of changed gallery'
-                            }
-    rv = client.patch(url+f'{temp_gallery.id}',
-                       headers={'authorization':'Bearer '+temp_account.generate_access_token()},
-                       json=changing_gallery_info)
+def test_gallery_patch_to_already_exist_gallery_name(client, create_temp_account, create_temp_gallery):
+    temp_account = create_temp_account()
+    temp_gallery1, temp_gallery2 = create_temp_gallery(temp_account),\
+                                   create_temp_gallery(temp_account)
 
-    assert rv.status_code == 403
+    rv = client.patch(url+f'{temp_gallery1.id}',
+                      headers={'authorization':'Bearer '+temp_account.generate_access_token()},
+                      json={'name':temp_gallery2.name})
 
+    assert rv.status_code == 409
