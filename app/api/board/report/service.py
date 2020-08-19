6ab3e56bc88afec99b12b3df5c0de5ce70cdf85b
EoJin <eojin.dev@gmail.com>
2020-08-20 03:28:39 +0900

    [CREATE] app/api/board/report/service.py

---
+++ app/api/board/report/service.py
@@ -0,0 +1,10 @@
+class PostReportService:
+    def deletePostReport():
+        pass
+
+
+class PostReportListService:
+    def create_post_report():
+        pass
+    def provide_post_report_list():
+        pass
