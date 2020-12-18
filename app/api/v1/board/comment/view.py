ef4dd4b17f13ae8dbcef33d124dbaa9b18baaaed
EoJin <eojin.dev@gmail.com>
2020-12-18 09:00:03 +0900

    MODIFY remove useless import

--- app/api/v1/board/comment/view.py
+++ app/api/v1/board/comment/view.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import make_response, abort, request
+from flask import abort, request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
