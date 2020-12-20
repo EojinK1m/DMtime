8bf695199fcb71fe70d3c17ed7471ea854e4265a
EoJin <eojin.dev@gmail.com>
2020-12-20 17:02:12 +0900

    MODIFY PostList get to do own job

--- app/api/v1/board/post/view.py
+++ app/api/v1/board/post/view.py
@@ -1,13 +1,55 @@
-from flask import make_response, request
+from flask import make_response, request, abort
 from flask_restful import Resource
 
+from app.util.request_validator import RequestValidator
+
 from app.api.v1.board.post.service import PostService, PostListService
-
-
+from app.api.v1.board.post.model import \
+    posts_schema,\
+    posts_schema_user,\
+    PostGetQueryParameterValidateSchema
+from app.api.v1.board.gallery.service import GalleryService
+from app.api.v1.user.account.service import AccountService
+from app.api.v1.user.service import UserService
 
 class PostList(Resource):
     def get(self):
-     return make_response(PostListService.provide_post_list())
+        RequestValidator.validate_request(PostGetQueryParameterValidateSchema(), request.args)
+
+        gallery_id = request.args.get(key='gallery-id', default=None, type=int)
+        username = request.args.get(key='username', default=None, type=str)
+        page = request.args.get(key='page', default=1, type=int)
+        per_page = request.args.get(key='per-page', default=20, type=int)
+
+        if gallery_id and username:
+            abort(400)
+
+        if gallery_id is None and username is None:
+          posts = PostListService.get_posts_with_paging(
+              per_page=per_page,
+              page=page
+          )
+          convert_schema = posts_schema
+        elif gallery_id is not None:
+            posts = PostListService.get_posts_by_gallery_with_paging(
+                gallery=GalleryService.get_gallery_by_id(gallery_id),
+                per_page=per_page,
+                page=page
+            )
+            convert_schema = posts_schema
+        elif username is not None:
+
+            posts = PostListService.get_posts_by_user_with_paging(
+                user=UserService.get_user_by_username(username),
+                per_page=per_page,
+                page=page
+            )
+            convert_schema = posts_schema_user
+
+        return {
+                   'posts': convert_schema.dump(posts.items),
+                   'number_of_pages': posts.pages
+               }, 200
 
     def post(self):
         gallery_id = request.args.get('gallery-id', None)
