fc3a3be52234b1286fd4b1c3350d8ae690802fad
EoJin <eojin.dev@gmail.com>
2021-01-06 11:48:00 +0900

    DELETE image_upload

--- app/api/v1/image/service.py
+++ app/api/v1/image/service.py
@@ -19,32 +19,6 @@ def get_path_from_filename(filename):
 
 class ImageService:
 
-
-    @staticmethod
-    def image_upload(image):
-        if not image:
-            return jsonify({'msg':'No image'}), 400
-
-        filename = image.filename
-        if filename == '':
-            return jsonify({'msg':'No selected file'}), 400
-
-        if not is_correct_image(filename):
-            return jsonify({'msg':'This image has tension that not supported'}), 400
-
-        image_column = ImageModel(filename='')
-        db.session.add(image_column)
-        db.session.commit()
-
-        saved_file_name = str(image_column.id) + '.' +filename.rsplit('.', 1)[1].lower()
-        image.save(os.path.join(app.config['IMAGE_UPLOADS'], saved_file_name))
-        image_column.filename = saved_file_name
-        db.session.commit()
-
-
-        return jsonify({'msg':'Upload succeed',\
-                        'image_info':ImageSchema(exclude=['filename']).dump(image_column)}), 200
-
     @classmethod
     def create_image(cls, file_name='', user_id=None, post_id=None, gallery_id=None):
         image = ImageModel(
