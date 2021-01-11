0a670f420c1cab0d81c1cc0a91947233b3e8c3e3
EoJin <eojin.dev@gmail.com>
2021-01-11 21:22:13 +0900

    UPDATE add attributes of account to UserModel to combine them.

--- app/api/v1/user/model.py
+++ app/api/v1/user/model.py
@@ -1,19 +1,41 @@
+import re
+from flask import current_app
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+from app import bcrypt
 from app import db, ma
+from app import jwt
 
 
 class UserModel(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.String(320), primary_key=True)
+    password_hash = db.Column(db.String(128), unique=False, nullable=False)
+
     username = db.Column(db.String(16), unique=True, nullable=False)
-    account_id = db.Column(db.Integer, db.ForeignKey('account.id', ondelete='CASCADE'), unique=True)
     explain = db.Column(db.Text, nullable=True)
 
     posts = db.relationship('PostModel', backref='uploader')
     profile_image = db.relationship('ImageModel', uselist=False)
     managing_gallery = db.relationship('GalleryModel', backref='manager')
+
     def delete_user(self):
         db.session.delete(self)
+
+    @staticmethod
+    def hash_password(password):
+        return bcrypt.generate_password_hash(password)
+
+    def generate_access_token(self, expire=None):
+        return create_access_token(identity=self.email, expires_delta=expire)
+
+    def generate_refresh_token(self, expire=None):
+        return create_refresh_token(identity=self.email, expires_delta=expire)
+
+    def verify_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
 
 class UserSchema(ma.SQLAlchemySchema):
     class Meta:
