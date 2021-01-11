8b8438eaf7aecceabd249eb1aba89eaac21be526
EoJin <eojin.dev@gmail.com>
2021-01-11 21:13:56 +0900

    UPDATE add check_is_school_email to email validate arg.

--- app/api/v1/auth/schema.py
+++ app/api/v1/auth/schema.py
@@ -1,8 +1,17 @@
 from app import ma
-from marshmallow import validate
+from marshmallow import validate, ValidationError
 
 
 class PostTokenValidateSchema(ma.Schema):
+
+    @staticmethod
+    def check_is_school_email(email):
+        email_domain = email.rsplit('@')[-1]
+
+        if not email_domain == 'dsm.hs.kr':
+            raise ValidationError('Email is not email of DSM.')
+
     password = ma.Str(required = True, validate = validate.Length(min = 8))
-    email = ma.Str(required = True, validate = validate.Email())
+    email = ma.Str(required = True, validate = [validate.Email(), check_is_school_email.__func__])
+
 
