671e3e571ef4369d0665791740dc69b6ea453926
EoJin <eojin.dev@gmail.com>
2021-01-11 21:14:22 +0900

    UPDATE add validate request line

--- app/api/v1/auth/view.py
+++ app/api/v1/auth/view.py
@@ -1,7 +1,16 @@
+from flask import Request
 from flask_restful import Resource
 
+from app.util.request_validator import RequestValidator
+
+from app.api.v1.auth.schema import PostTokenValidateSchema
+from app.api.v1.user.account.service import AccountService
 
 class Token(Resource):
 
     def post(self):
+        RequestValidator.validate_request(PostTokenValidateSchema(), Request.json)
+
+        # account_2_generate_token = AccountService.get_account_by_email
+
         pass
