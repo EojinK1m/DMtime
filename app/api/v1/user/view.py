7a9ec584706717134f5a7a7fb662d3ebeced2064
EoJin <eojin.dev@gmail.com>
2021-01-12 09:08:24 +0900

    MODIFY User get to do own work

--- app/api/v1/user/view.py
+++ app/api/v1/user/view.py
@@ -1,18 +1,19 @@
 from flask import make_response, request
 from flask_restful import Resource
 
-from app.api.v1.user.service import UserService
-from app.api.v1.user.service import AccountService, AuthService, DuplicateCheck
-
+from app.api.v1.user.service import UserService, AccountService, AuthService, DuplicateCheck
+from app.api.v1.user.model import user_schema
 
 
 
 class User(Resource):
+
     def get(self, username):
-        return make_response(UserService.provide_user_info(username))
+        return user_schema.dump(UserService.get_user_by_username(username)), 200
 
     def patch(self, username):
         return make_response(UserService.modify_user_info(username, request.json))
+
 
 class Account(Resource):
     def get(self):
@@ -24,25 +25,31 @@ class Account(Resource):
     def delete(self):
         return make_response(AccountService.delete_account(request.args.get('email')))
 
+
 class AccountPassword(Resource):
     def put(self):
         return make_response(AccountService.change_account_password(request.json))
+
 
 class Auth(Resource):
     def post(self):
         return make_response(AuthService.login(request.json))
 
+
 class Refresh(Resource):
     def get(self):
         return make_response(AuthService.refresh())
+
 
 class DuplicateCheckEmail(Resource):
     def get(self):
         return make_response(DuplicateCheck.email_check(request.args.get('email')))
 
+
 class DuplicateCheckUsername(Resource):
     def get(self):
         return make_response(DuplicateCheck.username_check(request.args.get('username')))
+
 
 class AuthEmailVerificationCode(Resource):
     def post(self):
