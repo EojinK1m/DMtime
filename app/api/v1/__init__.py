c5eac012b6aa2635bb285b6f0ee47125867f8ba2
eojin <eojin.dev@gmail.com>
2021-02-07 01:54:30 +0900

    UPDATE register db session handling to after request

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,8 +3,13 @@ from flask.blueprints import Blueprint
 from app.api.v1.image import image_api
 from app.api.v1.board import board_api
 from app.api.v1.user import user_api, account_api
+from app.api.v1.general import view
+
 
 v1_blueprint = Blueprint('api_v1', 'api_v1')
+
+v1_blueprint.after_request(view.commit_session_after_request)
+v1_blueprint.teardown_request(view.rollback_session_when_error)
 
 board_api.init_app(v1_blueprint)
 user_api.init_app(v1_blueprint)
