42d0a68d8752fefde184301dab3508ea69ee02c9
eojin <eojin.dev@gmail.com>
2021-02-08 22:40:39 +0900

    UPDATE implement Token post

--- app/api/v1/auth/view.py
+++ app/api/v1/auth/view.py
@@ -1,16 +1,25 @@
-from flask import Request
+from flask import request, abort
 from flask_restful import Resource
 
 from app.util.request_validator import RequestValidator
 
 from app.api.v1.auth.schema import PostTokenValidateSchema
-from app.api.v1.user.service import AccountService
+from app.api.v1.user.service import UserService
+
 
 class Token(Resource):
 
     def post(self):
-        RequestValidator.validate_request(PostTokenValidateSchema(), Request.json)
+        RequestValidator.validate_request(PostTokenValidateSchema(), request.json)
+        email = request.json.get('email')
+        password = request.json.get('password')
 
-        # account_2_generate_token = AccountService.get_account_by_email
+        login_user = UserService.get_user_by_email_or_None(email)
 
-        pass
+        if login_user and login_user.verify_password(password):
+            return {
+                'access_token':login_user.generate_access_token(),
+                'refresh_token':login_user.generate_refresh_token()
+            }, 200
+
+        abort(401, 'incorrect username or password')
