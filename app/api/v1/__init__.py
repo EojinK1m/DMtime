41e0f7469c7ea249490aedde06aa79e0ebc2d232
eojin <eojin.dev@gmail.com>
2021-02-08 22:57:03 +0900

    UPDATE register auth api to v1

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from flask.blueprints import Blueprint
 from app.api.v1.image import image_api
 from app.api.v1.board import board_api
 from app.api.v1.user import user_api, account_api
+from app.api.v1.auth import auth_api
 from app.api.v1.general import view
 
 
@@ -15,3 +16,4 @@ board_api.init_app(v1_blueprint)
 user_api.init_app(v1_blueprint)
 account_api.init_app(v1_blueprint)
 image_api.init_app(v1_blueprint)
+auth_api.init_app(v1_blueprint)
