90b9a56f4348eaba86ea4be58e40137022e2bdde
eojin <eojin.dev@gmail.com>
2021-02-09 00:02:42 +0900

    MODIFY add empty line

--- app/api/v1/general/view.py
+++ app/api/v1/general/view.py
@@ -5,6 +5,7 @@ def commit_session_after_request(response):
     db.session.commit()
     return response
 
+
 def rollback_session_when_error(error=None):
     print(f'rollback_session_when_error is executed \n arg is {error}')
 
