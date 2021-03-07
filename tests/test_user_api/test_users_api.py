56f83caaf3221479f499898fdda6ae70ebce5ee3
eojin <eojin.dev@gmail.com>
2021-03-07 23:09:24 +0900

    UPDATE add default_ready to users_api test

--- tests/test_user_api/test_users_api.py
+++ tests/test_user_api/test_users_api.py
@@ -1,4 +1,25 @@
+from pytest import fixture
+
 url = '/api/v1/users'
 
+@fixture
+def default_ready():
+    class DefaultReady:
+        def __init__(self):
+            self.uri = '/api/v1/users'
+            self.correct_register_json = {
+                'email':'testemail123@dsm.hs.kr',
+                'password':'',
+                'username':'회원가입test123'
+            }
+
+    return DefaultReady
+
+
+def test_users_post_correctly_response_200(client, default_ready):
+    rv = client.post(
+        default_ready.uri,
+        json=default_ready.correct_register_json
+    )
 
 pass
