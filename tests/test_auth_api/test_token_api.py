8a882b5d4b87c9072475d02ba5d59850f2824d1e
eojin <eojin.dev@gmail.com>
2021-03-10 23:12:21 +0900

    Create token_api test

---
+++ tests/test_auth_api/test_token_api.py
@@ -0,0 +1,35 @@
+from pytest import fixture
+
+@fixture
+def test_user(create_temp_account):
+    return create_temp_account()
+
+def login(client, json):
+    return client.post(
+        '/api/v1/token',
+        json = json
+    )
+
+def test_login_correctly_response_200_access_token(client, test_user):
+    rv = login(
+        client,
+        {
+            'email':test_user.email,
+            'password':'test_password_1'
+        }
+    )
+
+    assert rv.status_code == 200
+    assert rv.json['access_token']
+
+def test_login_failed_response_401(client, test_user):
+    rv = login(
+        client,
+        {
+            'email':test_user.email,
+            'password':'wrong_password1'
+        }
+    )
+
+    assert rv.status_code == 401
+
