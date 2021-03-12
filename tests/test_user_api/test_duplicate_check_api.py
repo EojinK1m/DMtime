d719d062f17f326bf5d27697d5852ca52d1c1149
eojin <eojin.dev@gmail.com>
2021-03-12 14:54:12 +0900

    Modify duplicate to duplication in test name

--- tests/test_user_api/test_duplicate_check_api.py
+++ tests/test_user_api/test_duplicate_check_api.py
@@ -28,7 +28,7 @@ def test_check_already_using_email_duplication_response_200_and_usable_false(cli
     assert rv.json['usable'] == False
 
 
-def test_email_duplicate_check_without_email_parameter_response_400(client):
+def test_email_duplication_check_without_email_parameter_response_400(client):
     rv = client.get(email_duplication_url)
 
     assert rv.status_code == 400
@@ -38,21 +38,21 @@ def check_username_duplication(client, username):
     return client.get(username_duplication_url + '?username=' + username)
 
 
-def test_check_not_using_username_duplicate_response_200_and_usable_true(client):
+def test_check_not_using_username_duplication_response_200_and_usable_true(client):
     rv = check_username_duplication(client, 'notusername')
 
     assert rv.status_code == 200
     assert rv.json['usable'] == True
 
 
-def test_check_using_username_duplicate_response_200_and_usable_false(client, test_user):
+def test_check_using_username_duplication_response_200_and_usable_false(client, test_user):
     rv = check_username_duplication(client, test_user.username)
 
     assert rv.status_code == 200
     assert rv.json['usable'] == False
 
 
-def test_email_duplicate_check_without_username_parameter(client):
+def test_email_duplication_check_without_username_parameter(client):
     rv = client.get(username_duplication_url)
 
     assert rv.status_code == 400
