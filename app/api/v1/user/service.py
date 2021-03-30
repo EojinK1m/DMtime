9ce8ff44f42f0f4ff67097dd5f59058b5e9dbdb9
eojinK1m <eojin.dev@gmail.com>
2021-03-30 18:21:18 +0900

    REFACTOR remove useless codes and use is None

--- app/api/v1/user/service.py
+++ app/api/v1/user/service.py
@@ -2,38 +2,20 @@ import json
 from smtplib import SMTPException
 from functools import wraps
 
-from flask import jsonify, abort, current_app
+from flask import abort, current_app
 from flask_jwt_extended import (
     get_jwt_identity,
-    jwt_required,
     verify_jwt_in_request,
-    jwt_refresh_token_required,
 )
 
 from app import db, redis_client, email_sender
 from app.util import verification_code_generater
 
-from app.api.v1.user.model import (
-    UserModel,
-    user_schema,
-    account_schema,
-    AccountChangePasswordInputSchema,
-    AccountRegisterSchema,
-)
+from app.api.v1.user.model import UserModel
 from app.api.v1.image.service import ImageService
 
 
 class UserService:
-    @staticmethod
-    def provide_user_info(username):
-        find_user = UserModel.query.filter_by(username=username).first()
-        if not find_user:
-            return jsonify({"msg": "user not found"}), 404
-
-        return jsonify(
-            {"msg": "query succeed", "user_info": user_schema.dump(find_user)}
-        )
-
     @staticmethod
     def get_user_by_username(username):
         find_user = UserModel.query.filter_by(username=username).first()
@@ -70,67 +52,6 @@ class UserService:
         return user
 
     @staticmethod
-    @jwt_required
-    def modify_user_info(username, data):
-        from app.api.v1.user.model import UserPatchInputSchema
-
-        error = UserPatchInputSchema().validate(data)
-        if error:
-            return jsonify(msg="Bad request, json body is wrong"), 400
-
-        user = (UserModel.query.filter_by(email=get_jwt_identity()).first()).user
-        if not user or not UserModel.query.filter_by(username=username).first():
-            return jsonify({"msg": "user not found"}), 404
-        elif user.username != username:
-            return jsonify({"msg": f"access denied, you are not {username}"}), 403
-
-        new_username = data.get("username", None)
-        new_explain = data.get("user_explain", None)
-        new_profile_image_id = data.get(
-            "profile_image_id", user.profile_image.id if user.profile_image else None
-        )
-
-        if not (new_username == None):
-            if UserModel.query.filter_by(username=new_username).first():
-                return jsonify(msg="Bad request, same username exist"), 400
-            user.username = new_username
-        if not (new_explain == None):
-            user.explain = new_explain
-        if not UserService.set_profile_image(user, new_profile_image_id):
-            return (
-                jsonify(
-                    {
-                        "msg": f"image not found, image {new_profile_image_id}is not exist"
-                    }
-                ),
-                404,
-            )
-
-        db.session.commit()
-
-        return jsonify({"msg": "modification succeed"}), 200
-
-    @staticmethod
-    def set_profile_image(user, profile_image_id):
-        print(user.id)
-        print(profile_image_id)
-
-        if user.profile_image == None:
-            if profile_image_id == None:
-                return True
-        elif user.profile_image.id == profile_image_id:
-            return True
-
-        if user.profile_image:
-            if not ImageService.delete_image(user.profile_image.id):
-                return False
-        if profile_image_id:
-            if not ImageService.set_foreign_key(profile_image_id, user.id, "user"):
-                return False
-
-        return True
-
-    @staticmethod
     def delete_user(user):
         profile_image = user.profile_image
 
@@ -163,45 +84,6 @@ class UserService:
 
 class AccountService:
     @staticmethod
-    @jwt_required
-    def provide_account_info():
-        email = get_jwt_identity()
-        account = UserModel.get_user_by_email(email)
-
-        return (
-            jsonify(account_info=account_schema.dump(account), msg="query succeed"),
-            200,
-        )
-
-    @staticmethod
-    def register_account(data):
-        AccountService.validate_account_register_data(data)
-
-        email = data.get("email")
-        AccountService.check_exist_same_email(email)
-        AccountService.check_exist_same_username(data.get("username"))
-
-        verification_code = AccountService.generate_verification_code()
-
-        AccountService.store_register_data_temporally(verification_code, data)
-        AccountService.send_verification_by_email(
-            verification_code=verification_code, to_send_email=email
-        )
-
-        return (
-            jsonify(
-                message="register account temporally succeed, plz verify requested email"
-            ),
-            201,
-        )
-
-    @staticmethod
-    def validate_account_register_data(data):
-        errors = AccountRegisterSchema().validate(data)
-        if errors:
-            abort(400, str(errors))
-
-    @staticmethod
     def check_exist_same_username(username):
         if UserModel.query.filter_by(username=username).first():
             abort(409, "same username exist")
@@ -211,7 +93,6 @@ class AccountService:
         if UserModel.query.filter_by(email=email).first():
             abort(409, "same email exist")
 
-    @staticmethod
     def store_register_data_temporally(verification_code, data):
         with redis_client.pipeline() as pipe:
             pipe.mset({verification_code: json.dumps(data)})
@@ -237,20 +118,6 @@ class AccountService:
                 return temp_code
 
     @staticmethod
-    def verify_email_verification_code(verification_code):
-        AccountService.validate_verification_code(verification_code)
-        register_data = AccountService.find_register_data_by_verification_code(
-            verification_code
-        )
-
-        AccountService.check_exist_same_username(register_data.get("username"))
-        AccountService.check_exist_same_email(register_data.get("email"))
-        AccountService.store_account_user_information(register_data)
-
-        redis_client.delete(verification_code)
-        return jsonify(msg="Verify succeed"), 200
-
-    @staticmethod
     def validate_verification_code(code):
         if not (verification_code_generater.validate_verification_code(code)):
             raise abort(400)
@@ -260,51 +127,14 @@ class AccountService:
         AccountService.validate_verification_code(verification_code)
         register_data = redis_client.get(verification_code)
 
-        if register_data == None:
+        if register_data is None:
             abort(404, "Verification code not found.")
 
         return json.loads(register_data.decode("utf-8"))
 
     @staticmethod
-    def store_account_user_information(register_data):
-        try:
-            new_account = UserModel(
-                email=register_data.get("email"),
-                password_hash=UserModel.hash_password(register_data.get("password")),
-            )
-            db.session.add(new_account)
-            db.session.flush()
-
-            new_user = UserModel(
-                username=register_data.get("username"), account=new_account
-            )
-            db.session.add(new_user)
-
-            db.session.commit()
-        except:
-            db.session.rollback()
-            abort(500, "an error occurred while adding infos in db")
-
-    @staticmethod
-    @jwt_required
-    def delete_account(email):
-        account = UserModel.get_user_by_email(email)
-        if not account:
-            abort(404, "Account not found")
-
-        if not get_jwt_identity() == email:
-            abort(403, "Access denied")
-
-        UserService.delete_user(account.user)
-        account.delete_account()
-
-        db.session.commit()
-        return jsonify(msg="account deleted!"), 200
-
-    @staticmethod
-    @jwt_required
     def change_account_password(user, password):
-        user.password_hash = UserModel.password_hash(password)
+        user.password_hash = user.hash_password(password)
         
     @staticmethod
     def find_user_by_email(email):
@@ -314,65 +144,3 @@ class AccountService:
             abort(404, "Account not found, there is no account include the email.")
 
         return found_account
-
-
-class AuthService:
-    @staticmethod
-    def login(data):
-        from app.api.v1.user.model import AccountLoginInputSchema
-
-        error = AccountLoginInputSchema().validate(data)
-        if error:
-            return jsonify(msg="Bad request, wrong json body"), 400
-
-        email = data.get("email", None)
-        password = data.get("password", None)
-
-        login_account = UserModel.get_user_by_email(email)
-
-        if login_account:
-            if login_account.verify_password(password):
-                access_token = login_account.generate_access_token()
-                refresh_token = login_account.generate_refresh_token()
-
-                return (
-                    jsonify(
-                        {
-                            "access_token": access_token,
-                            "refresh_token": refresh_token,
-                            "msg": "login succeed",
-                        }
-                    ),
-                    200,
-                )
-
-        return jsonify({"msg": "incorrect username or password"}), 401
-
-    @staticmethod
-    @jwt_refresh_token_required
-    def refresh():
-        email = get_jwt_identity()
-        account = UserModel.get_user_by_email(email)
-        access_token = account.generate_access_token()
-
-        return jsonify({"access_token": access_token}), 200
-
-
-class DuplicateCheck:
-    @staticmethod
-    def email_check(email):
-        if email == None:
-            abort(400, "email parameter missed")
-        if UserModel.get_user_by_email(email) == None:
-            return jsonify({"msg": "same email does not exist", "usable": True}), 200
-        else:
-            return jsonify({"msg": "same email exist", "usable": False}), 200
-
-    @staticmethod
-    def username_check(username):
-        if username == None:
-            abort(400, "username parameter missed")
-        if UserModel.query.filter_by(username=username).first() == None:
-            return jsonify({"msg": "same username does not exist", "usable": True}), 200
-        else:
-            return jsonify({"msg": "same username exist", "usable": False}), 200
