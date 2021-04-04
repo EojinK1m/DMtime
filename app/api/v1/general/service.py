ed7bbbc56d771930f311c6a12f41ce40d89e2f24
eojinK1m <eojin.dev@gmail.com>
2021-04-05 08:53:21 +0900

    CREATE general service, add admin required

---
+++ app/api/v1/general/service.py
@@ -0,0 +1,19 @@
+from flask import abort
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_claims
+
+
+def verify_admin_jwt_in_request():
+    verify_jwt_in_request()
+
+    claims = get_jwt_claims()
+    if not claims["roles"] == "admin":
+        abort(403, "Access denied, admin only!")
+
+
+def admin_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_admin_jwt_in_request()
+        return fn(*args, **kwargs)
+
+    return wrapper
