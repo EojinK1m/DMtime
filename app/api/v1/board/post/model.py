0b7eada7ef5bc6614063a733eb5b60055f59f329
eojinK1m <eojin.dev@gmail.com>
2021-04-05 16:00:15 +0900

    CREATE PostDislikeModel

--- app/api/v1/board/post/model.py
+++ app/api/v1/board/post/model.py
@@ -53,6 +53,20 @@ class PostLikeModel(db.Model):
         nullable=False,
     )
 
+class PostDislikeModel(db.Model):
+    __tablename__ = "postdislike"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    post_id = db.Column(
+        db.Integer(),
+        db.ForeignKey("post.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    liker_id = db.Column(
+        db.String(320),
+        db.ForeignKey("user.email", ondelete="CASCADE"),
+        nullable=False,
+    )
 
 class PostSchema(ma.SQLAlchemySchema):
     class Meta:
