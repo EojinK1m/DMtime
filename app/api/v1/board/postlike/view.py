9829b266f177b9e999143adb285b44d58100bcb5
eojinK1m <eojin.dev@gmail.com>
2021-04-05 23:59:24 +0900

    CREATE pseudocode for postlikeAPIs

--- app/api/v1/board/postlike/view.py
+++ app/api/v1/board/postlike/view.py
@@ -1,37 +1,59 @@
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 
-from app.api.v1.board.post.service import PostService
-
 class PostLike(Resource):
     @jwt_required
     def post(self, post_id):
-        post = PostService.get_post_by_post_id(post_id)
-        request_account = AccountService.find_user_by_email(get_jwt_identity())
+        '''
+            validate_post_id
 
-        postlike = PostLikeService.get_postlike_by_post_and_account(
-            post=post, account=request_account
-        )
-
-        if postlike:
-            PostLikeService.delete_postlike(postlike)
-            message = "Cancel post like"
-        else:
-            PostLikeService.create_postlike(account=request_account, post=post)
-            message = "Like post"
-
-        db.session.commit()
-        return {"message": message, "likes": len(post.postlikes)}, 201
+            check_postlike_or_postdislike_already_exist
+                if exist abort 404
+            
+            create postlike by user
+            response 201, number_of_likes
+        '''
 
     @jwt_required
     def delete(self, post_id):
+        '''
+            validate_post_id
+
+            check_is_postlike_by_request_user_exist
+                if not exist abort 404
+                else get postlike
+            
+            delete postlike
+            
+            response 200, number_of_likes
+        '''
         pass
 
 
 class PostDislike(Resource):
     def post(self, post_id):
+        '''
+            validate_post_id
+
+            check_postlike_or_postdislike_already_exist
+                if exist abort 404
+            
+            create postlike by user
+            response 201, number_of_likes
+        '''
         pass
     
     @jwt_required
     def delete(self, post_id):
+        '''
+            validate_post_id
+
+            check_is_postlike_by_request_user_exist
+                if not exist abort 404
+                else get postlike
+            
+            delete postlike
+            
+            response 200, number_of_likes
+        '''
         pass
