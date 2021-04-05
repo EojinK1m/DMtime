9bb08e362608f2eccf92d5069f6491255b26a848
eojinK1m <eojin.dev@gmail.com>
2021-04-05 12:00:43 +0900

    CREATE test for postdislikeAPI

--- tests/test_postlike_api/test_postlike_api.py
+++ tests/test_postlike_api/test_postlike_api.py
@@ -20,7 +20,7 @@ def post_postlike(client):
 def post_postdislike(client):
     def _post_postdislike(access_token, post_id):
         return client.post(
-            create_dislike_uri(post_id),
+            create_postdislike_uri(post_id),
             headers={"authorization": "Bearer " + access_token}
         )
     
@@ -66,7 +66,7 @@ def temp_user(create_temp_account):
 def temp_postlike(create_temp_postlike, temp_post, temp_user):
     postlike = create_temp_postlike(
         liker_id=temp_user.email,
-        post_id=temp_post
+        post_id=temp_post.id
     )
 
     return {
@@ -90,12 +90,12 @@ def temp_postdislike(create_temp_postdislike, temp_post, temp_user):
 
 def test_post_postlike_succeess_response_201(
     post_postlike,
-    test_post,
-    test_user
+    temp_post,
+    temp_user
 ):    
     rv = post_postlike(
-        test_user.access_token,
-        test_post.id
+        temp_user.generate_access_token(),
+        temp_post.id
     )
 
     assert rv.status_code == 201
@@ -147,26 +147,59 @@ def test_delete_postlike_when_postlike_is_not_exist_reponse_404(
 
 ###TEST postdislike API
 
-def test_post_postdislike_success_reponse_201(
+def test_post_postdislike_succeess_response_201(
+    post_postdislike,
     temp_post,
     temp_user
-):
-    pass
+):    
+    rv = post_postdislike(
+        access_token=temp_user.generate_access_token(),
+        post_id=temp_post.id
+    )
 
-def test_post_postdislike_when_already_like_exist_response_409(
-    temp_post,
-    temp_user
+    assert rv.status_code == 201
+
+def test_post_postdislike_when_already_postlike_exist_response_409(
+    post_postdislike,
+    temp_postlike
 ):
-    pass
+    rv = post_postdislike(
+        access_token=temp_postlike['user'].generate_access_token(),
+        post_id=temp_postlike['post'].id
+    )
+    
+    assert rv.status_code == 409
 
 def test_post_postdislike_when_already_postdislike_exist_response_409(
-    temp_post,
-    temp_user
+    temp_postdislike,
+    post_dispostlike
 ):
-    pass
+    rv = post_postdislike(
+        access_token=temp_postdislike['user'].generate_access_token(),
+        post_id=temp_postdislike['post'].id
+    )
 
-def test_delete_postdislike_when_postdislike_is_not_exist_response_404(
-    temp_post,
-    temp_user
+    assert rv.status_code == 409
+
+    
+def test_delete_postdislike_success_response_200(
+    temp_postdislike,
+    delete_postdislike
 ):
-    pass
+    rv = delete_postdislike(
+        temp_postdislike['user'].generate_access_token(),
+        temp_postdislike['post'].id
+    )
+
+    assert rv.status_code == 200
+
+def test_delete_postdislike_when_postdislike_is_not_exist_reponse_404(
+    temp_post,
+    temp_user,
+    delete_postdislike
+):
+    rv = delete_postdislike(
+        post_id=temp_post.id,
+        access_token=temp_user.generate_access_token()
+    )
+    assert rv.status_code == 404
