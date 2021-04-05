08b0468269171a2be6d19ef4b9460949cd1422f1
eojinK1m <eojin.dev@gmail.com>
2021-04-05 23:59:54 +0900

    CREATE skeletons of postlike services

--- app/api/v1/board/postlike/service.py
+++ app/api/v1/board/postlike/service.py
@@ -1,1 +1,30 @@
-pass
+
+
+class PostlikeService():
+    
+    def get_postlike_by_user_id(self, user_id):
+        pass
+
+    def abort_404_if_not_exist_postlike_by_user(self, user_id):
+        pass
+
+    def abort_409_if_postlike_by_user_already_exist(self, user_id):
+        pass
+
+    def create_postlike(self, user_id):
+        pass
+
+
+class PostdislikeService():
+
+    def get_postdislike_by_user_id(self, user_id):
+        pass
+
+    def abort_404_if_not_exist_postdislike_by_user(self, user_id):
+        pass
+    
+    def abort_409_if_postdislike_by_user_already_exist(self, user_id):
+        pass
+
+    def create_postdislike(self, user_id):
+        pass
