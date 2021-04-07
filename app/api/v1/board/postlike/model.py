71c860a79c3c9f85e15ded2b787bad9a2931c735
eojinK1m <eojin.dev@gmail.com>
2021-04-07 15:53:59 +0900

    UPDATE remove id attribute from postlike models

--- app/api/v1/board/postlike/model.py
+++ app/api/v1/board/postlike/model.py
@@ -1,31 +1,41 @@
 from app import db
 
+
 class PostlikeModel(db.Model):
     __tablename__ = "postlike"
 
-    id = db.Column(db.Integer(), primary_key=True)
     post_id = db.Column(
         db.Integer(),
         db.ForeignKey("post.id", ondelete="CASCADE"),
-        nullable=False,
+        primary_key=True,
     )
     liker_id = db.Column(
         db.String(320),
         db.ForeignKey("user.email", ondelete="CASCADE"),
-        nullable=False
+        primary_key=True
     )
-    
+    # included_post = db.relationship(
+    #     'PostModel',
+    #     back_populates='dislikes'
+    # )
+    # included_post = db.relationship('PostModel', back_populates="likes")
+
 class PostdislikeModel(db.Model):
     __tablename__ = "postdislike"
 
-    id = db.Column(db.Integer(), primary_key=True)
     post_id = db.Column(
         db.Integer(),
         db.ForeignKey("post.id", ondelete="CASCADE"),
-        nullable=False,
+        primary_key=True
     )
     liker_id = db.Column(
         db.String(320),
         db.ForeignKey("user.email", ondelete="CASCADE"),
-        nullable=False,
+        primary_key=True
     )
+
+    # included_post = db.relationship(
+    #     'PostModel',
+    #     back_populates='dislikes'
+    # )
+    # backref=db.backref('teams_membership', cascade='delete, delete-orphan')
