e944c304ad58ec8f0bd33994e42f9aaed7945af6
eojinK1m <eojin.dev@gmail.com>
2021-04-08 18:34:42 +0900

    UPDATE implement postlikeAPI services

--- app/api/v1/board/postlike/service.py
+++ app/api/v1/board/postlike/service.py
@@ -1,30 +1,91 @@
+from flask import abort
+
+from app import db
+
+from app.api.v1.board.postlike.model import PostlikeModel, PostdislikeModel
 
 
 class PostlikeService():
+    def __init__(self):
+        self.postlike_repository = PostlikeRepository()
+
+    @classmethod
+    def get_postlike(cls, user_id, post_id):
+        return PostlikeModel.query\
+            .filter_by(liker_id=user_id)\
+            .filter_by(post_id=post_id)\
+            .first()
     
-    def get_postlike_by_user_id(self, user_id):
-        pass
+    @classmethod
+    def get_postlike_or_abort_404(cls, user_id, post_id):
+        postlike = cls.get_postlike(user_id, post_id)
 
-    def abort_404_if_not_exist_postlike_by_user(self, user_id):
-        pass
+        if postlike is None:
+            abort(404)
+        
+        return postlike
+            
+    @classmethod
+    def get_postlikes_by_post_id(cls, post_id):
+        return PostlikeModel.query.filter_by(post_id=post_id).all()
+        # return self.postlike_repository.get_postlikes_by_post_id(post_id)
 
-    def abort_409_if_postlike_by_user_already_exist(self, user_id):
-        pass
+    @classmethod
+    def abort_404_if_not_exist_postlike_by_user_on_post(cls, user_id, post_id):
+        if cls.get_postlike(user_id, post_id) is None:
+            abort(404)
 
-    def create_postlike(self, user_id):
-        pass
+    @classmethod
+    def abort_409_if_postlike_exists(cls, user_id, post_id):
+        if cls.get_postlike(user_id, post_id):
+            abort(409)
+
+    @classmethod
+    def create_postlike(cls, user_id, post_id):
+        new_postlike = PostlikeModel(
+            liker_id=user_id,
+            post_id=post_id
+        )
+
+        db.session.add(new_postlike)
+        return new_postlike
 
 
 class PostdislikeService():
 
-    def get_postdislike_by_user_id(self, user_id):
-        pass
-
-    def abort_404_if_not_exist_postdislike_by_user(self, user_id):
-        pass
+    @classmethod
+    def get_postdislike(cls, user_id, post_id):
+        return PostdislikeModel.query\
+            .filter_by(liker_id=user_id)\
+            .filter_by(post_id=post_id)\
+            .first()
     
-    def abort_409_if_postdislike_by_user_already_exist(self, user_id):
-        pass
 
-    def create_postdislike(self, user_id):
-        pass
+    @classmethod
+    def get_postdislike_or_abort_404(cls, user_id, post_id):
+        postdislike = cls.get_postdislike(user_id, post_id)
+
+        if postdislike is None:
+            abort(404)
+        
+        return postdislike
+    
+    @classmethod
+    def get_postdislikes_by_post_id(cls, post_id):
+        return PostdislikeModel.query.filter_by(post_id=post_id).all()
+
+    @classmethod
+    def abort_409_if_postdislike_exists(cls, user_id, post_id):
+        if cls.get_postdislike(user_id, post_id):
+            abort(409)
+
+    @classmethod
+    def create_postdislike(cls, user_id, post_id):
+        new_postdislike = PostdislikeModel(
+            liker_id=user_id,
+            post_id=post_id
+        )
+
+        db.session.add(new_postdislike)
+        
+        return new_postdislike
