993c8ac306be71fd3228b05094da753cbffd7797
eojinK1m <eojin.dev@gmail.com>
2021-04-08 18:35:08 +0900

    UPDATE implement postlikeAPI view

--- app/api/v1/board/postlike/view.py
+++ app/api/v1/board/postlike/view.py
@@ -1,3 +1,4 @@
+from flask import abort
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
@@ -12,30 +13,44 @@ from app.api.v1.board.postlike.schema import (
     RequestPostlikeApiQueryParameterVaidateSchema
 )
 
+
+def validate_post_id(post_id):
+    RequestValidator.validate_request(
+        data={"post_id": post_id},
+        schema=RequestPostlikeApiQueryParameterVaidateSchema()
+    )
+
+
 class PostLike(Resource):
 
     def __init__(self, *args, **kwargs):
         self.postlike_service = PostlikeService()
+        self.postdislike_service = PostdislikeService()
+        
     #     self.postlike_service = kwargs["post_like_service"]
     #     self.reuqest_validator = kwargs.get('request_validator')
     #     self.post_id_validate_schema = kwargs.get('post_id_validate_schema')
-
-    def validate_post_id(self, post_id):
-        RequestValidator.validate_request(
-            data={"post_id": post_id},
-            schema=RequestPostlikeApiQueryParameterVaidateSchema()
-        )
     # def get_user_from_jwt()
 
     @jwt_required
     def post(self, post_id):
         request_user = UserService.get_user_by_email_or_none(get_jwt_identity())
-        self.validate_post_id(post_id)
-        
-        if self.postlike_service.get_postlike_by_user_id(request_user.email):
-            abort(409)
-        
-        self.postlike_service.create_postlike(request_user.email)
+        validate_post_id(post_id)
+
+        self.postlike_service.abort_409_if_postlike_exists(
+            user_id=request_user.email,
+            post_id=post_id
+        )
+        self.postdislike_service.abort_409_if_postdislike_exists(
+            user_id=request_user.email,
+            post_id=post_id
+        )
+
+        self.postlike_service.create_postlike(
+            post_id=post_id,
+            user_id=request_user.email
+        )
+
         number_of_likes = \
             len(self.postlike_service.get_postlikes_by_post_id(post_id))
 
@@ -53,6 +68,19 @@ class PostLike(Resource):
 
     @jwt_required
     def delete(self, post_id):
+        request_user = UserService.get_user_by_email_or_none(get_jwt_identity())
+        validate_post_id(post_id)
+
+        postlike_to_delete = \
+            self.postlike_service.get_postlike_or_abort_404(
+                user_id=request_user.email,
+                post_id=post_id
+            )
+        
+        postlike_to_delete.delete()
+
+        return {}, 200
+
         '''
             validate_post_id
 
@@ -69,11 +97,35 @@ class PostLike(Resource):
 
 class PostDislike(Resource):
     
-    def validaet_post_id(post_id):
-        pass    
+    def __init__(self, *args, **kwargs):
+        self.postdislike_service = PostdislikeService()
+        self.postlike_service = PostlikeService()
 
     @jwt_required
     def post(self, post_id):
+        request_user = UserService.get_user_by_email_or_none(get_jwt_identity())
+        validate_post_id(post_id)
+
+        self.postlike_service.abort_409_if_postlike_exists(
+            user_id=request_user.email,
+            post_id=post_id
+        )
+        self.postdislike_service.abort_409_if_postdislike_exists(
+            user_id=request_user.email,
+            post_id=post_id
+        )
+
+        self.postdislike_service.create_postdislike(
+            post_id=post_id,
+            user_id=request_user.email
+        )
+
+        number_of_dislikes = \
+            len(self.postdislike_service.get_postdislikes_by_post_id(post_id))
+
+        return {number_of_dislikes: number_of_dislikes}, 201
+
+
         '''
             validate_post_id
 
@@ -87,6 +139,19 @@ class PostDislike(Resource):
     
     @jwt_required
     def delete(self, post_id):
+        request_user = UserService.get_user_by_email_or_none(get_jwt_identity())
+        validate_post_id(post_id)
+
+        postdislike_to_delete = \
+            self.postdislike_service.get_postdislike_or_abort_404(
+                user_id=request_user.email,
+                post_id=post_id
+            )
+        
+        postdislike_to_delete.delete()
+
+        return {}, 200
+
         '''
             validate_post_id
 
