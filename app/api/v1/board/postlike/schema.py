948a64c0be740d8f0af8e21cb2f5c76b30a98245
eojinK1m <eojin.dev@gmail.com>
2021-04-08 18:38:56 +0900

    REFACTOR  remove blank

--- app/api/v1/board/postlike/schema.py
+++ app/api/v1/board/postlike/schema.py
@@ -2,8 +2,8 @@ from app import ma
 
 from marshmallow.validate import Range
 
+
 class RequestPostlikeApiQueryParameterVaidateSchema(ma.Schema):
     post_id = ma.Integer(
         required=True, validate=Range(min=0)
     )
-    
