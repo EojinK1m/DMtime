f25b03053363fce16e0cea94db318c09f490193e
eojink1m <eojin.dev@gmail,com>
2021-04-22 16:02:11 +0900

    CREATE reports api package and models

---
+++ app/api/v1/board/report/model.py
@@ -0,0 +1,59 @@
+from app import db
+
+
+class CommentReport(db.Model):
+    __tablename__ = "comment_report"
+
+    id = db.Column(db.Integer, primary_key=True)
+    reason = db.Column(db.Integer(), nullable=False)
+    detail_reason = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(
+        db.String(320),
+        db.ForeignKey("user.email", ondelete="CASCADE"),
+        nullable=False,
+    )
+    gallery_id = db.Column(
+        db.String(30),
+        db.ForeignKey("gallery.gallery_id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    post_id = db.Column(
+        db.Integer(),
+        db.ForeignKey("post.id", ondelete="SET NULL"),
+        nullable=True,
+    )
+    comment_id = db.Column(
+        db.Integer(),
+        db.ForeignKey("comment.id", ondelete="SET NULL"),
+        nullable=True,
+    )
+
+    reporter = db.relationship("UserModel")
+    reported_comment = db.relationship("PostModel")
+
+
+class PostReport(db.Model):
+    __tablename__ = "post_report"
+
+    id = db.Column(db.Integer, primary_key=True)
+    reason = db.Column(db.Integer(), nullable=False)
+    detail_reason = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(
+        db.String(320),
+        db.ForeignKey("user.email", ondelete="CASCADE"),
+        nullable=False,
+    )
+    gallery_id = db.Column(
+        db.String(30),
+        db.ForeignKey("gallery.gallery_id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    post_id = db.Column(
+        db.Integer(),
+        db.ForeignKey("post.id", ondelete="SET NULL"),
+        nullable=True,
+    )
+
+    reporter = db.relationship("UserModel")
+    reported_post = db.relationship("PostModel")
+
