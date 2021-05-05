f6c47528e11ff59c44e3188913f560199435528a
eojink1m <eojin.dev@gmail,com>
2021-05-05 20:41:30 +0900

    UPDATE comment report API resource들 route

--- app/api/v1/board/__init__.py
+++ app/api/v1/board/__init__.py
@@ -2,25 +2,28 @@ from flask_restful import Api
 
 from app.api.v1.board.post.view import Post, PostList, HotPostList
 from app.api.v1.board.gallery.view import Gallery, GalleryList
+from .report.view import CommentReport, CommentReports, CommentReportsNestedInGallery
 from app.api.v1.board.gallery.report.view import Report, ReportList
 from app.api.v1.board.comment.view import Comment, CommentList
 from app.api.v1.board.postlike.view import PostLike, PostDislike
 
 board_api = Api()
 
-board_api.add_resource(Post, "/board/posts/<post_id>")
-board_api.add_resource(PostLike, "/board/posts/<post_id>/like")
-board_api.add_resource(PostDislike, "/board/posts/<post_id>/dislike")
-board_api.add_resource(PostList, "/board/posts")
-board_api.add_resource(HotPostList, "/board/posts/hot")
+resource_routes = {
+    "/board/posts/<post_id>": Post,
+    "/board/posts/<post_id>/like": PostLike,
+    "/board/posts/<post_id>/dislike": PostDislike,
+    "/board/posts": PostList,
+    "/board/posts/hot": HotPostList,
+    "/board/galleries/<gallery_id>": Gallery,
+    "/board/galleries": GalleryList,
+    "/board/comments/<comment_id>": Comment,
+    "/board/comments": CommentList,
+    "/board/galleries/<gallery_id>/reports/<report_id>": Report,
+    "/board/galleries/<gallery_id>/comment-reports": CommentReportsNestedInGallery,
+    "/comment-reports/<report_id>": CommentReport,
+    "/comment-reports": CommentReports
+}
 
-board_api.add_resource(Gallery, "/board/galleries/<gallery_id>")
-board_api.add_resource(GalleryList, "/board/galleries")
-
-board_api.add_resource(Comment, "/board/comments/<comment_id>")
-board_api.add_resource(CommentList, "/board/comments")
-
-board_api.add_resource(
-    Report, "/board/galleries/<gallery_id>/reports/<report_id>"
-)
-board_api.add_resource(ReportList, "/board/galleries/<gallery_id>/reports")
+for path, resource in resource_routes.items():
+    board_api.add_resource(resource, path)
