8f13f0ed1bdd71cab6301049d460556da26b924f
eojink1m <eojin.dev@gmail,com>
2021-05-31 11:36:32 +0900

    UPDATE add get_user_from_token to general service

--- app/api/v1/general/service.py
+++ app/api/v1/general/service.py
@@ -1,7 +1,10 @@
 from flask import abort
-from flask_jwt_extended import verify_jwt_in_request, get_jwt_claims
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_claims, jwt_required, get_jwt_identity
 from functools import wraps
 
+from ..user.repository import UserRepository
+
+user_repository = UserRepository()
 
 def verify_admin_jwt_in_request():
     verify_jwt_in_request()
@@ -18,3 +21,9 @@ def admin_required(fn):
         return fn(*args, **kwargs)
 
     return wrapper
+
+
+@jwt_required
+def get_user_from_token():
+    return user_repository.get_user_by_email(get_jwt_identity())
+
